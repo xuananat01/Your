cbbe0ebb9d3952eef6c50a4e1ac912bb6b35b44e
nguyenxuanan3221 <nguyenxuanan3221@gmail.com>
2024-01-29 08:39:56 +0700

    [UPDATE] login with phone on firebase

--- src/screens/LoginScreen/LoginPhone.js
+++ src/screens/LoginScreen/LoginPhone.js
@@ -1,59 +1,134 @@
+import React, {useState, useEffect} from 'react';
 import {
-  StyleSheet,
+  SafeAreaView,
   Text,
   TextInput,
   TouchableOpacity,
   View,
 } from 'react-native';
-import React, {useCallback, useState} from 'react';
 import auth from '@react-native-firebase/auth';
+import { useNavigation } from '@react-navigation/native';
 
 const LoginPhone = () => {
+  const [user, setUser] = useState(null);
+
+  const [mobile, setMobile] = useState(null);
+
   const [confirm, setConfirm] = useState(null);
+
   const [code, setCode] = useState('');
-  const [phone, setPhone] = useState('');
 
-  const getOTP = useCallback(async () => {
-    try {
-      const data = await auth().signInWithPhoneNumber(`+84${phone}`, true);
-      setConfirm(data);
-    } catch (err) {
-      console.log(err);
-    }
-  }, [phone]);
+  const navigation = useNavigation()
 
-  const confirmOTP = useCallback(async () => {
-    try {
-      const res = await confirm.confirm(code);
-      console.log(res);
-    } catch (err) {
-      console.log(err);
+  const onAuthStateChanged = async userAuth => {
+    if (!userAuth) {
+      return;
     }
-  }, [confirm, setCode]);
+    if (userAuth) {
+      console.log(userAuth);
+      setUser(userAuth);
+      navigation.navigate('weatherScreen')
+    }
+
+    return () => userReference();
+  };
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return () => {
+      subscriber;
+    };
+  }, []);
+
+  const signInWithMobileNumber = async () => {
+    const confirmation = await auth().signInWithPhoneNumber(mobile);
+    setConfirm(confirmation);
+  };
+
+  const confirmCode = async () => {
+    try {
+      await confirm.confirm(code);
+    } catch (error) {
+      console.log('Invalid code.');
+    }
+  };
+
+  const signOut = async () => {
+    auth().signOut();
+
+    setUser(null);
+
+    return () => userReference();
+  };
 
   return (
-    <View style={styles.phoneNumber}>
-      <TextInput
-        aria-label="Phone Number"
-        maxLength={10}
-        onChangeText={setPhone}
-        value={phone}
-        keyboardType="phone-pad"
-      />
-      {confirm ? <TextInput style={{marginTop: 2}} aria-label="OTP" onChangeText={setCode} /> : null}
-      <TouchableOpacity onPress={confirm ? confirmOTP : getOTP}>
-        {confirm ? <Text>Confirm OTP</Text> : <Text>Get OTP</Text>}
-      </TouchableOpacity>
-    </View>
+    <SafeAreaView style={{alignItems: 'center', flex: 1, marginTop: 100}}>
+      <View style={{margin: 10}}>
+        <Text>Mobile Sign In Tutorial</Text>
+      </View>
+
+      <View style={{margin: 10}}>
+        {user === null && (
+          <>
+            <TextInput
+              value={mobile}
+              onChangeText={e => setMobile(e)}
+              placeholder="mobile"
+              style={{
+                borderWidth: 1,
+                margin: 10,
+                padding: 10,
+                width: 200,
+              }}></TextInput>
+            {!confirm ? (
+              <>
+                <TouchableOpacity
+                  style={{
+                    borderWidth: 1,
+                    margin: 10,
+                    padding: 10,
+                    alignItems: 'center',
+                  }}
+                  onPress={() => signInWithMobileNumber()}>
+                  <Text>Get Code</Text>
+                </TouchableOpacity>
+              </>
+            ) : (
+              <>
+                <TextInput
+                  value={code}
+                  onChangeText={e => setCode(e)}
+                  placeholder="Code"
+                  style={{
+                    borderWidth: 1,
+                    margin: 10,
+                    padding: 10,
+                    width: 200,
+                  }}></TextInput>
+                <TouchableOpacity
+                  style={{
+                    borderWidth: 1,
+                    margin: 10,
+                    padding: 10,
+                    alignItems: 'center',
+                  }}
+                  onPress={() => confirmCode()}>
+                  <Text>Confirm Code</Text>
+                </TouchableOpacity>
+              </>
+            )}
+          </>
+        )}
+      </View>
+      {user !== null && (
+        <View style={{margin: 10}}>
+          <Text style={{margin: 10}}>{user.phoneNumber}</Text>
+          <TouchableOpacity onPress={signOut} style={{alignItems: 'center'}}>
+            <Text>Sign Out</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+    </SafeAreaView>
   );
 };
 
 export default LoginPhone;
-
-const styles = StyleSheet.create({
-  phoneNumber: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-  },
-});
